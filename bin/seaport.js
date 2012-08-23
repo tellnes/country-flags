fdb9e588b7f102d781a899eb1ef3769836a6090e
Christian Tellnes <christian@tellnes.no>
2012-08-23 20:47:57 +0200

    add seaport env support

--- bin/seaport.js
+++ bin/seaport.js
@@ -2,5 +2,33 @@ var cf = require('../')
   , seaport = require('seaport')
   , pkg = require('../package.json')
 
-seaport .connect.apply(seaport, process.argv.slice(2))
+var ports
+
+var args = []
+
+if (process.env.SEAPORT_HOST && process.env.SEAPORT_PORT) {
+  args.push(process.env.SEAPORT_HOST, process.env.SEAPORT_PORT)
+
+} else if (process.env.SEAPORT_PORT) {
+  args.push(process.env.SEAPORT_PORT)
+
+} else if (process.env.SEAPORT) {
+  args.push(process.env.SEAPORT)
+
+}
+
+if (args.length) {
+  if (process.env.SEAPORT_SECRET) {
+    args.push( { secret: process.env.SEAPORT_SECRET })
+  }
+} else {
+  args = process.argv.slice(2)
+}
+
+if (!args.length) {
+  console.error('Missing seaport connection details')
+  return
+}
+
+seaport .connect.apply(seaport, args)
         .service(pkg.name + '@' + pkg.version, cf.listen)
